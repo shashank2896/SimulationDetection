465eab416ae111c8829b104f90f60913247d2b97
shashank2896 <shashank2896@gmail.com>
2018-11-22 15:01:40 -0500

    Add files via upload

---
+++ res_last_conv_op.py
@@ -0,0 +1,43 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import resnet
+
+plt.ion()   # interactive mode
+
+#data loading
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = 'images'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train']}
+class_names = image_datasets['train'].classes
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+model = resnet.resnet18()
+model.load_state_dict(torch.load('./saved_model.pth'))
+model.eval()
